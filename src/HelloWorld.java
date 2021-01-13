9c2eecc076586b4da279aa1c1f7f9dcbaed6b02a
nathankurz <nathankurz91@gmail.com>
2021-01-13 12:45:39 -0500

    Added name line Added comments

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,19 @@
-// Developer: Halena Watson
+//Author: Halena Wetton
+//Date: 1-13-2021
+//File: HelloWorld.java
+
+//Simple program that prints a message to the screen
 public class HelloWorld {
+
+    //Main method
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("My name is Halena Wetton");
-    }
-}
+
+        //Invoke the println method from the System.out, which
+        //requires one argument, the item to be printed
+        //to the screen
+        System.out.println("Hello World!");
+        System.out.println("My name is Halena Wetton.");
+
+    }//closing main()
+
+}//closing class
